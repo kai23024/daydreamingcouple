32cc5c0de390f904b1b8b7595b551960ea366d6b
kai23024 <kai23024@gmail.com>
2026-08-19 22:27:40 +0800

    內文字體改為微軟正黑體，保留其他系統與 Noto Sans TC 作為備援

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ export default <Partial<Config>>{
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Noto Sans TC"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        sans: ['"Microsoft JhengHei"', '"PingFang TC"', '"Noto Sans TC"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
         serif: ['"Noto Serif TC"', 'ui-serif', 'Georgia', 'serif']
       },
       colors: {
